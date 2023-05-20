4d77b3f99b589a10c9c8b9dd51cd8b7ff90e0da1
Yadunand Prem <yadunand@yadunut.com>
2023-05-20 10:27:24 +0800

    feat: add basic todos list

--- app/(home)/index.jsx
+++ app/(home)/index.jsx
@@ -1,10 +1,22 @@
-import { View } from 'react-native';
-import { Button, Text } from 'react-native-paper';
+import { FlatList, View } from 'react-native';
 import { supabase } from '../../lib/supabase';
+import { useEffect, useState } from 'react';
+import { Text } from 'react-native-paper';
 
 export default function HomeScreen() {
+    const [todos, setTodos] = useState([]);
+
+    async function fetchTodos() {
+        let { data } = await supabase.from('todos').select('*');
+        setTodos(data);
+    }
+
+    useEffect(() => {
+        fetchTodos();
+    }, []);
     return (
         <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+            <FlatList data={todos} renderItem={({ item }) => <Text>{item.task}</Text>} />
         </View>
     );
 }
