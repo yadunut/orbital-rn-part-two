b321af943bb941e37a28817d8cb352627b5e799e
Yadunand Prem <yadunand@yadunut.com>
2023-05-20 09:54:50 +0800

    feat: add login and register

--- app/(auth)/register.jsx
+++ app/(auth)/register.jsx
@@ -1,16 +1,15 @@
-import { View } from "react-native";
 import { useState } from "react";
-import { Text, TextInput, Button, ActivityIndicator } from "react-native-paper";
-import { Link } from "expo-router";
 import { supabase } from "../../lib/supabase";
+import { View } from "react-native";
+import { Text, TextInput, ActivityIndicator, Button } from 'react-native-paper';
 
-export default function LoginPage() {
+export default function Register() {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
     const [loading, setLoading] = useState(false);
     const [errMsg, setErrMsg] = useState('');
+
     const handleSubmit = async () => {
-        setErrMsg('');
         if (email == '') {
             setErrMsg("email cannot be empty")
             return;
@@ -20,13 +19,14 @@ export default function LoginPage() {
             return;
         }
         setLoading(true);
-        const { error } = await supabase.auth.signInWithPassword({ email, password });
+        const { error } = await supabase.auth.signUp({ email, password });
         setLoading(false);
         if (error) {
             setErrMsg(error.message);
             return;
         }
     }
+
     return (
         <View style={{ flex: 1, justifyContent: 'center' }}>
             <Text>Email</Text>
@@ -45,9 +45,6 @@ export default function LoginPage() {
             <Button onPress={handleSubmit}>Submit</Button>
             {errMsg !== "" && <Text>{errMsg}</Text>}
             {loading && <ActivityIndicator />}
-            <Link href="/register">
-                <Button>Go to register</Button>
-            </Link>
         </View>
-    )
+    );
 }
